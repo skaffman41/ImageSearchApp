0f76ff0653f9f72a6c2bce618f79cdce0438b0a5
Alex Nimas <mail@alexnimas.ru>
2020-10-18 18:53:17 +0300

    Added GalleryFragment and navigation graph

---
+++ app/src/main/java/ru/alexnimas/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package ru.alexnimas.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import ru.alexnimas.imagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
